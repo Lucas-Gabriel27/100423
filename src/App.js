9bd3fe26b2a0c870e06fd8a08b75f22548435bff
Aluno <aluno@etec.sp.gov.br>
2023-04-10 12:21:38 -0300

    situacao commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import MediaNotas from './componentes/medianotas';
+import'./componentes/medianotas.css'
+import Situacao from './componentes/situacao'
+import Imagem from './componentes/Imagem.js'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Situacao></Situacao>
+      <Imagem></Imagem>
     </div>
   );
 }
 
+
 export default App;
+
+
